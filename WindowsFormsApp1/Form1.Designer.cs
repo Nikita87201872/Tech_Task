2faeb43f40d7da4b3b936d7f586ee2734dbfb4c7
Nikita87201872 <sukorskynikita@gmail.com>
2024-01-10 18:22:55 +0300

    first

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.CubeButton = new System.Windows.Forms.Button();
+            this.PyramidButton = new System.Windows.Forms.Button();
+            this.ParallelepipedButton = new System.Windows.Forms.Button();
+            this.X_Coordinate = new System.Windows.Forms.Button();
+            this.Y_Coordinate = new System.Windows.Forms.Button();
+            this.Z_Coordinate = new System.Windows.Forms.Button();
+            this.textBoxRotationAngle = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.UpdateDataGrid = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 150);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(554, 452);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // CubeButton
+            // 
+            this.CubeButton.Location = new System.Drawing.Point(12, 12);
+            this.CubeButton.Name = "CubeButton";
+            this.CubeButton.Size = new System.Drawing.Size(134, 59);
+            this.CubeButton.TabIndex = 1;
+            this.CubeButton.Text = "Cube";
+            this.CubeButton.UseVisualStyleBackColor = true;
+            this.CubeButton.Click += new System.EventHandler(this.CubeButton_Click);
+            // 
+            // PyramidButton
+            // 
+            this.PyramidButton.Location = new System.Drawing.Point(152, 12);
+            this.PyramidButton.Name = "PyramidButton";
+            this.PyramidButton.Size = new System.Drawing.Size(134, 59);
+            this.PyramidButton.TabIndex = 2;
+            this.PyramidButton.Text = "Pyramid";
+            this.PyramidButton.UseVisualStyleBackColor = true;
+            this.PyramidButton.Click += new System.EventHandler(this.PyramidButton_Click);
+            // 
+            // ParallelepipedButton
+            // 
+            this.ParallelepipedButton.Location = new System.Drawing.Point(292, 12);
+            this.ParallelepipedButton.Name = "ParallelepipedButton";
+            this.ParallelepipedButton.Size = new System.Drawing.Size(134, 59);
+            this.ParallelepipedButton.TabIndex = 3;
+            this.ParallelepipedButton.Text = "Parallelepiped";
+            this.ParallelepipedButton.UseVisualStyleBackColor = true;
+            this.ParallelepipedButton.Click += new System.EventHandler(this.ParallelepipedButton_Click);
+            // 
+            // X_Coordinate
+            // 
+            this.X_Coordinate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.X_Coordinate.Location = new System.Drawing.Point(754, 500);
+            this.X_Coordinate.Name = "X_Coordinate";
+            this.X_Coordinate.Size = new System.Drawing.Size(60, 60);
+            this.X_Coordinate.TabIndex = 4;
+            this.X_Coordinate.Text = "X";
+            this.X_Coordinate.UseVisualStyleBackColor = true;
+            this.X_Coordinate.Click += new System.EventHandler(this.X_Coordinate_Click);
+            // 
+            // Y_Coordinate
+            // 
+            this.Y_Coordinate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Y_Coordinate.Location = new System.Drawing.Point(820, 500);
+            this.Y_Coordinate.Name = "Y_Coordinate";
+            this.Y_Coordinate.Size = new System.Drawing.Size(60, 60);
+            this.Y_Coordinate.TabIndex = 5;
+            this.Y_Coordinate.Text = "Y";
+            this.Y_Coordinate.UseVisualStyleBackColor = true;
+            this.Y_Coordinate.Click += new System.EventHandler(this.Y_Coordinate_Click);
+            // 
+            // Z_Coordinate
+            // 
+            this.Z_Coordinate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Z_Coordinate.Location = new System.Drawing.Point(886, 500);
+            this.Z_Coordinate.Name = "Z_Coordinate";
+            this.Z_Coordinate.Size = new System.Drawing.Size(60, 60);
+            this.Z_Coordinate.TabIndex = 6;
+            this.Z_Coordinate.Text = "Z";
+            this.Z_Coordinate.UseVisualStyleBackColor = true;
+            this.Z_Coordinate.Click += new System.EventHandler(this.Z_Coordinate_Click);
+            // 
+            // textBoxRotationAngle
+            // 
+            this.textBoxRotationAngle.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxRotationAngle.Location = new System.Drawing.Point(792, 118);
+            this.textBoxRotationAngle.Name = "textBoxRotationAngle";
+            this.textBoxRotationAngle.Size = new System.Drawing.Size(155, 26);
+            this.textBoxRotationAngle.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Location = new System.Drawing.Point(792, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 22);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Angle rotate";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(572, 150);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(375, 344);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // UpdateDataGrid
+            // 
+            this.UpdateDataGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.UpdateDataGrid.Location = new System.Drawing.Point(572, 500);
+            this.UpdateDataGrid.Name = "UpdateDataGrid";
+            this.UpdateDataGrid.Size = new System.Drawing.Size(134, 59);
+            this.UpdateDataGrid.TabIndex = 11;
+            this.UpdateDataGrid.Text = "Update";
+            this.UpdateDataGrid.UseVisualStyleBackColor = true;
+            this.UpdateDataGrid.Click += new System.EventHandler(this.UpdateDataGrid_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(958, 614);
+            this.Controls.Add(this.UpdateDataGrid);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxRotationAngle);
+            this.Controls.Add(this.Z_Coordinate);
+            this.Controls.Add(this.Y_Coordinate);
+            this.Controls.Add(this.X_Coordinate);
+            this.Controls.Add(this.ParallelepipedButton);
+            this.Controls.Add(this.PyramidButton);
+            this.Controls.Add(this.CubeButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Location = new System.Drawing.Point(15, 15);
+            this.MinimumSize = new System.Drawing.Size(650, 430);
+            this.Name = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Button UpdateDataGrid;
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+
+        private System.Windows.Forms.Label label1;
+
+        private System.Windows.Forms.TextBox textBoxRotationAngle;
+
+        private System.Windows.Forms.Button X_Coordinate;
+        private System.Windows.Forms.Button Y_Coordinate;
+        private System.Windows.Forms.Button Z_Coordinate;
+
+        private System.Windows.Forms.Button PyramidButton;
+        private System.Windows.Forms.Button ParallelepipedButton;
+
+        private System.Windows.Forms.Button CubeButton;
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+
+        #endregion
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,472 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public class CoordinateIn3D
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Z { get; set; }
+
+        public CoordinateIn3D(float x, float y, float z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+        public PointF Project(float distance)
+        {
+            float[,] transformMatrix = {
+                { 1, 0, 0 },
+                { 0, 1, 0 },
+                { 0, 0, distance / (distance - Z) }
+            };
+
+            float newX = transformMatrix[0, 0] * X + transformMatrix[0, 1] * Y + transformMatrix[0, 2] * Z;
+            float newY = transformMatrix[1, 0] * X + transformMatrix[1, 1] * Y + transformMatrix[1, 2] * Z;
+
+            return new PointF(newX, newY);
+        }
+    }
+  public partial class Form1 : Form
+  {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeDataGridView();
+
+            this.Resize += Form1_Resize;
+
+            this.MouseWheel += new MouseEventHandler(Form1_MouseWheel);
+            
+            textBoxRotationAngle.Text = "10";
+            
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            
+            AddMouseHandlers();
+            
+            AddKeyHandlers();
+            
+            pictureBox1.BackColor = Color.White;
+        }
+        
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            RedrawFigure();
+        }
+        
+        private void AddKeyHandlers()
+        {
+            this.KeyDown -= Form1_KeyDown;
+            this.KeyDown += Form1_KeyDown;
+            this.Focus();
+            pictureBox1.TabStop = true;
+            this.KeyPreview = true;
+        }
+
+        private void AddMouseHandlers()
+        {
+            pictureBox1.MouseDown -= pictureBox1_MouseDown;
+            pictureBox1.MouseMove -= pictureBox1_MouseMove;
+            pictureBox1.MouseUp -= pictureBox1_MouseUp;
+            pictureBox1.MouseDown += pictureBox1_MouseDown;
+            pictureBox1.MouseMove += pictureBox1_MouseMove;
+            pictureBox1.MouseUp += pictureBox1_MouseUp;
+        }
+        
+        private void InitializeDataGridView()
+        {
+            dataGridView1.ColumnCount = 4;
+
+            dataGridView1.Columns[0].Name = "X";
+            dataGridView1.Columns[1].Name = "Y";
+            dataGridView1.Columns[2].Name = "Z";
+            dataGridView1.Columns[3].Name = "Point index";
+
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.MultiSelect = false;
+        }
+
+        private void UpdateDataGridView()
+        {
+            dataGridView1.Rows.Clear();
+
+            for (int i = 0; i < figurePoints.Count; i++)
+            {
+                dataGridView1.Rows.Add(figurePoints[i].X, figurePoints[i].Y, figurePoints[i].Z, $"Point {i + 1}");
+            }
+        }
+
+        private List<CoordinateIn3D> figurePoints;
+        
+        private bool figureOnPictureBox = false;
+
+        public void FigureDraw()
+        {
+            float distance = 300;
+            
+            Pen p = new Pen(Color.Black);
+            
+            Graphics graphics = Graphics.FromImage(pictureBox1.Image);
+            
+            graphics.TranslateTransform(pictureBox1.Width / 2, pictureBox1.Height / 2);
+
+            GraphicsPath graphicsPath = new GraphicsPath();
+
+            PointF[] points = new PointF[figurePoints.Count];
+            
+            for (int i = 0; i < figurePoints.Count; i++)
+            {
+                points[i] = figurePoints[i].Project(distance);
+            }
+            
+            graphicsPath.AddPolygon(points);
+            
+            graphics.DrawPolygon(p, points);
+
+            pictureBox1.Invalidate();
+        }
+        
+        public void CubeDraw()
+        {
+            int x = pictureBox1.Height / 5;
+            int y = pictureBox1.Height / 5;
+            int z = pictureBox1.Height / 5;
+            List<CoordinateIn3D> cubePoints = new List<CoordinateIn3D>()
+            {
+                new CoordinateIn3D(-x, -y, -z),
+                new CoordinateIn3D(x, -y, -z),
+                new CoordinateIn3D(x, y, -z),
+                new CoordinateIn3D(-x, y, -z),
+                new CoordinateIn3D(-x, -y, -z),
+                
+                new CoordinateIn3D(-x, -y, z),
+                new CoordinateIn3D(x, -y, z),
+                new CoordinateIn3D(x, y, z),
+                new CoordinateIn3D(-x, y, z),
+                new CoordinateIn3D(-x, -y, z),
+                new CoordinateIn3D(-x, y, z),
+                new CoordinateIn3D(-x, y, -z),
+                
+                new CoordinateIn3D(x, y, -z),
+                new CoordinateIn3D(x, y, z),
+                new CoordinateIn3D(x, -y, z),
+                new CoordinateIn3D(x, -y, -z),
+            };
+
+            figurePoints = cubePoints;
+        }
+        
+        private void CubeButton_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            CubeDraw();
+            FigureDraw();
+            figureOnPictureBox = true;
+            UpdateDataGridView();
+        }
+
+        public void PyramidDraw()
+        {
+            int x = pictureBox1.Height / 5;
+            int y = pictureBox1.Height / 5;
+            int z = pictureBox1.Height / 5;
+            List<CoordinateIn3D> pyramidPoints = new List<CoordinateIn3D>()
+            {
+                new CoordinateIn3D(-x, y, -z),
+                new CoordinateIn3D(x, y, -z),
+                new CoordinateIn3D(x, y, z),
+                new CoordinateIn3D(-x, y, z),
+                new CoordinateIn3D(0, -2 * y, 0),
+                new CoordinateIn3D(x, y, -z),
+                new CoordinateIn3D(x, y, z),
+                new CoordinateIn3D(0, -2 * y, 0),
+                new CoordinateIn3D(-x, y, -z),
+                new CoordinateIn3D(-x, y, z)
+            };
+
+            figurePoints = pyramidPoints;
+        }
+        
+        private void PyramidButton_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            PyramidDraw();
+            FigureDraw();
+            figureOnPictureBox = true;
+            UpdateDataGridView();
+        }
+        
+        public void ParallelepipedDraw()
+        {
+            int x = pictureBox1.Height / 5;
+            int y = pictureBox1.Height / 7 * 2;
+            int z = pictureBox1.Height / 5;
+            List<CoordinateIn3D> parallelepipedPoints = new List<CoordinateIn3D>()
+            {
+                new CoordinateIn3D(-x, -y, -z),
+                new CoordinateIn3D(x, -y, -z),
+                new CoordinateIn3D(x, y, -z),
+                new CoordinateIn3D(-x, y, -z),
+                new CoordinateIn3D(-x, -y, -z),
+                
+                new CoordinateIn3D(-x, -y, z),
+                new CoordinateIn3D(x, -y, z),
+                new CoordinateIn3D(x, y, z),
+                new CoordinateIn3D(-x, y, z),
+                new CoordinateIn3D(-x, -y, z),
+                new CoordinateIn3D(-x, y, z),
+                new CoordinateIn3D(-x, y, -z),
+                
+                new CoordinateIn3D(x, y, -z),
+                new CoordinateIn3D(x, y, z),
+                new CoordinateIn3D(x, -y, z),
+                new CoordinateIn3D(x, -y, -z),
+            };
+
+            figurePoints = parallelepipedPoints;
+        }
+
+        private void ParallelepipedButton_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            ParallelepipedDraw();
+            FigureDraw();
+            figureOnPictureBox = true;
+            UpdateDataGridView();
+        }
+
+        private void RedrawFigure()
+        {
+            if (figureOnPictureBox)
+            {
+                pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+                FigureDraw();
+            }
+        }
+
+        public void X_Rotate(float angle)
+        {
+            Graphics graphics = Graphics.FromImage(pictureBox1.Image);
+            graphics.TranslateTransform(pictureBox1.Width / 2, pictureBox1.Height / 2);
+
+            for (int i = 0; i < figurePoints.Count; i++)
+            {
+                float x = figurePoints[i].X;
+                float y = figurePoints[i].Y;
+                float z = figurePoints[i].Z;
+
+                float newY = y * (float)Math.Cos(angle) - z * (float)Math.Sin(angle);
+                float newZ = z * (float)Math.Cos(angle) + y * (float)Math.Sin(angle);
+
+                figurePoints[i] = new CoordinateIn3D(x, newY, newZ);
+            }
+            RedrawFigure();
+        }
+
+        private void X_Coordinate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                float angle = float.Parse(textBoxRotationAngle.Text) * (float)Math.PI / 180;
+                X_Rotate(angle);
+            }
+            catch (Exception exception)
+            {
+                HandlingError(exception);
+            }
+        }
+
+        public void Y_Rotate(float angle)
+        {
+            Graphics graphics = Graphics.FromImage(pictureBox1.Image);
+            graphics.TranslateTransform(pictureBox1.Width / 2, pictureBox1.Height / 2);
+
+            for (int i = 0; i < figurePoints.Count; i++)
+            {
+                float x = figurePoints[i].X;
+                float y = figurePoints[i].Y;
+                float z = figurePoints[i].Z;
+
+                float newX = x * (float)Math.Cos(angle) + z * (float)Math.Sin(angle);
+                float newZ = z * (float)Math.Cos(angle) - x * (float)Math.Sin(angle);
+
+                figurePoints[i] = new CoordinateIn3D(newX, y, newZ);
+            }
+            RedrawFigure();
+        }
+        
+        private void Y_Coordinate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                float angle = float.Parse(textBoxRotationAngle.Text) * (float)Math.PI / 180;
+                Y_Rotate(angle);
+            }
+            catch (Exception exception)
+            {
+                HandlingError(exception);
+            }
+        }
+
+        public void Z_Rotate(float angle)
+        {
+            Graphics graphics = Graphics.FromImage(pictureBox1.Image);
+            graphics.TranslateTransform(pictureBox1.Width / 2, pictureBox1.Height / 2);
+
+            for (int i = 0; i < figurePoints.Count; i++)
+            {
+                float x = figurePoints[i].X;
+                float y = figurePoints[i].Y;
+                float z = figurePoints[i].Z;
+
+                float newX = x * (float)Math.Cos(angle) - y * (float)Math.Sin(angle);
+                float newY = y * (float)Math.Cos(angle) + x * (float)Math.Sin(angle);
+
+                figurePoints[i] = new CoordinateIn3D(newX, newY, z);
+            }
+            RedrawFigure();
+        }
+        
+        private void Z_Coordinate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                float angle = float.Parse(textBoxRotationAngle.Text) * (float)Math.PI / 180;
+                Z_Rotate(angle);
+            }
+            catch (Exception exception)
+            {
+                HandlingError(exception);
+            }
+        }
+
+        private bool isMouseDown = false;
+        private Point lastMousePos;
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isMouseDown = true;
+            lastMousePos = e.Location;
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isMouseDown = false;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                if (isMouseDown)
+                {
+                    int deltaX = e.X - lastMousePos.X;
+                    int deltaY = e.Y - lastMousePos.Y;
+
+                    float angleX = deltaX * 0.01f;
+                    float angleY = deltaY * 0.01f;
+
+                    X_Rotate(-angleY);
+                    Y_Rotate(angleX);
+
+                    lastMousePos = e.Location;
+                }
+            }
+            catch (Exception exception)
+            {
+                HandlingError(exception);
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            float angle = 0.05f;
+            if (!textBoxRotationAngle.Focused)
+            {
+                try
+                {
+                    switch (e.KeyCode)
+                    {
+                        case Keys.W:
+                            X_Rotate(angle);
+                            break;
+                        case Keys.S:
+                            X_Rotate(-angle);
+                            break;
+                        case Keys.D:
+                            Y_Rotate(angle);
+                            break;
+                        case Keys.A:
+                            Y_Rotate(-angle);
+                            break;
+                    }
+                }
+                catch (Exception exception)
+                {
+                    HandlingError(exception);
+                }
+            }
+        }
+        
+        private void HandlingError(Exception exception)
+        {
+            isMouseDown = false;
+            try
+            {
+                float value = float.Parse(textBoxRotationAngle.Text);
+                if (textBoxRotationAngle.Text == "")
+                {
+                    MessageBox.Show(@"Введите угол поворота!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    MessageBox.Show(@"Вы не вызвали фигуру!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Введён неверный формат числа!(Пример: 1,01)", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void FigureScaling(float changeSize)
+        {
+            for (int i = 0; i < figurePoints.Count; i++)
+            {
+                figurePoints[i].X *= changeSize;
+                figurePoints[i].Y *= changeSize;
+                figurePoints[i].Z *= changeSize;
+            }
+
+            RedrawFigure();
+        }
+        
+        private void Form1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            if (pictureBox1.Bounds.Contains(e.Location) && figureOnPictureBox)
+            {
+                int delta = e.Delta;
+                float changeSize = (delta > 0) ? 1.01f : 0.99f;
+                FigureScaling(changeSize);
+            }
+        }
+
+        private void UpdateDataGrid_Click(object sender, EventArgs e)
+        {
+            UpdateDataGridView();
+        }
+  }
+}
